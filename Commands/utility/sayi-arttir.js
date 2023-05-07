30ce3d73b6aca450e44ccfdf3ae705337379d775
NoM84 <126786961+NoM84@users.noreply.github.com>
2023-05-07 15:51:49 +0300

    Update sayi-arttir.js

--- Commands/utility/sayi-arttir.js
+++ Commands/utility/sayi-arttir.js
@@ -5,11 +5,21 @@ const {
 } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("test-sayi-arttir"),
+  data: new SlashCommandBuilder()
+    .setName("test-sayi-arttir")
+    .setDescription("Test town count arttırıcı."),
   /**
    *
    * @param {ChatInputCommandInteraction} interaction
-   * @param {*} client
+   * @param {Client} client
    */
-  async exectue(interaction, client) {},
+  async exectue(interaction, client) {
+    const mongoose = client.database;
+    const TownNumberSchema = client.databaseSchemas.townNumber;
+    const TownNumber = mongoose.model("Town-Count", TownNumberSchema);
+    let townNumber = TownNumber.findOne({ id: "Main" });
+    townNumber.TotalTowns += 1;
+    townNumber.save();
+    interaction.reply("Arttırıldı.");
+  },
 };
